9f4c870d6b9996bb88bbf909561a650fcbd99091
jounghoolee <jounghoolee@hanmail.net>
2015-07-07 21:45:32 +0900

    재학 실수?
    main.c 에 flag 키고 encoder 출력 죽인거 말구 그위에 cam run2 왜 살아 있는거니

--- Roboin_CAMwithBTMR_commentMerge/FLASH/Sources/myuart_c.d
+++ Roboin_CAMwithBTMR_commentMerge/FLASH/Sources/myuart_c.d
@@ -1,33 +1,33 @@
-Sources/myuart_c.obj: C:\Google\ Drive\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\myuart.c \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\lib_ewl.prefix \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\common.prefix \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_c_version.h \
-	C:\Google\ Drive\workspace\Roboin_CAMwithBTMR_commentMerge\Prefix\MPC5606B_FLASH_VLE.prefix \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdio.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ansi_parms.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewlGlobals.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\pa\ansi_prefix.PA_EABI.bare.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\os_enum.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdio \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stddef.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstddef \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\null.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\size_t.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\wchar_t.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_lib_ext1.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_rsize_t.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\eof.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\va_list.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\file_struc.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdio_api.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdarg.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdarg \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\pa\stdarg.EPPC.h \
-	C:\Google\ Drive\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\myuart.h \
-	C:\Google\ Drive\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\jdp.h \
-	C:\Google\ Drive\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\typedefs.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdint.h \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdint \
-	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\limits_api.h \
-	C:\Google\ Drive\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\ngv_dmu.h \
-	C:\Google\ Drive\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\rappid_utils.h 
+Sources/myuart_c.obj: C:\Users\jounghoo\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\myuart.c \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\lib_ewl.prefix \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\common.prefix \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_c_version.h \
+	C:\Users\jounghoo\workspace\Roboin_CAMwithBTMR_commentMerge\Prefix\MPC5606B_FLASH_VLE.prefix \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdio.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ansi_parms.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewlGlobals.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\pa\ansi_prefix.PA_EABI.bare.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\os_enum.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdio \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stddef.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstddef \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\null.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\size_t.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\wchar_t.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_lib_ext1.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_rsize_t.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\eof.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\va_list.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\file_struc.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdio_api.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdarg.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdarg \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\pa\stdarg.EPPC.h \
+	C:\Users\jounghoo\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\myuart.h \
+	C:\Users\jounghoo\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\jdp.h \
+	C:\Users\jounghoo\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\typedefs.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdint.h \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdint \
+	D:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\limits_api.h \
+	C:\Users\jounghoo\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\ngv_dmu.h \
+	C:\Users\jounghoo\workspace\Roboin_CAMwithBTMR_commentMerge\Sources\rappid_utils.h 
