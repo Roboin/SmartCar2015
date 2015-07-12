db05530c38ec1aae4934eadc15d02a94caca4f8e
JaeHak Jeong <jeongjh823@yonsei.ac.kr>
2015-07-13 03:21:08 +0900

    Roboin, Librarly, LaneDetecting Migrated Testbed
    앞으로 Robin_NEW로 수정해가면 됨. LCD 응용해도 좋을듯.

---
+++ Roboin_NEW_TESTmod/FLASH/Sources/intc_sw_vecttable_c.d
@@ -0,0 +1,62 @@
+Sources/intc_sw_vecttable_c.obj: C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\intc_sw_vecttable.c \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\lib_ewl.prefix \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\common.prefix \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_c_version.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Prefix\MPC5606B_FLASH_VLE.prefix \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\rappid_ref.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\jdp.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\typedefs.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdint.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ansi_parms.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewlGlobals.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\pa\ansi_prefix.PA_EABI.bare.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\os_enum.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdint \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\limits_api.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_lib_ext1.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\sys_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\intc_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\sysclk_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\linflex_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\adc_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\msr_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\swt_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\edma_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\i2c_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\flexcan_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\cansp_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\emios_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\dspi_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\siu_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\mpu_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\rgm_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\pit_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\stm_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\rtc_init.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\flash_init.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdio.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdio \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stddef.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstddef \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\null.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\size_t.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\wchar_t.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_rsize_t.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\eof.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\va_list.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\file_struc.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdio_api.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdlib.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdlib \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\div_t.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\gpio.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\adc_drv.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\timer.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\UART_drv_api.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\lcd.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\camera.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\encoder.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\motor.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\onBoardLedSwitch.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\ultra_sonic.h \
+	C:\Freescale\workspace\Roboin_NEW_TESTmod\Sources\isr_init.h 
