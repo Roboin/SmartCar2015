0e154d90183d444cb8ef29d894f22af3b882e997
JaeHak Jeong <jeongjh823@yonsei.ac.kr>
2015-07-14 21:14:17 +0900

    modjh merged with conering tune
    s

---
+++ Roboin_NEW_Cornering_Tune2113/FLASH/Sources/crt0_s.d
@@ -0,0 +1,4 @@
+Sources/crt0_s.obj: C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\crt0.s \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\crt0_include.inc \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\ecc_init.inc \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\crt0.s 
---
+++ Roboin_NEW_Cornering_Tune2113/FLASH/Sources/intc_sw_vecttable_c.d
@@ -0,0 +1,62 @@
+Sources/intc_sw_vecttable_c.obj: C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\intc_sw_vecttable.c \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\lib_ewl.prefix \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\common.prefix \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_c_version.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Prefix\MPC5606B_FLASH_VLE.prefix \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\rappid_ref.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\jdp.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\typedefs.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdint.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ansi_parms.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewlGlobals.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\pa\ansi_prefix.PA_EABI.bare.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\os_enum.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdint \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\limits_api.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_lib_ext1.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\sys_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\intc_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\sysclk_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\linflex_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\adc_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\msr_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\swt_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\edma_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\i2c_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\flexcan_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\cansp_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\emios_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\dspi_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\siu_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\mpu_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\rgm_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\pit_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\stm_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\rtc_init.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\flash_init.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdio.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdio \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stddef.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstddef \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\null.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\size_t.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\wchar_t.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_rsize_t.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\eof.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\va_list.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\file_struc.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdio_api.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\stdlib.h \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\cstdlib \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\div_t.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\gpio.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\adc_drv.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\timer.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\UART_drv_api.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\lcd.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\camera.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\encoder.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\motor.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\onBoardLedSwitch.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\ultra_sonic.h \
+	C:\Freescale\workspace\Roboin_NEW_Cornering_Tune\Sources\isr_init.h 
